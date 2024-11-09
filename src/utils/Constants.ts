4fd986dc968e6603711e32c960a05c60bbf57395
Soifouani <75247310+Soifouani@users.noreply.github.com>
2024-11-09 23:10:58 +0100

    Mise en place d'un composant modal pour gérer la redirection vers des url externe.

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -4,3 +4,6 @@ export const FREE_TRADE = "Zones de Gratuité";
 export const ALL_PROJECTS = "Tous nos Projets";
 export const EVENTS = "Evènements";
 export const ASSOCIATION = "Vie Associative";
+
+export const HELLO_ASSO_MODAL_TEXT = "En cliquant sur confirmer, nous vous redirigeons vers notre page Nord Est Toulousain en Transition directement sur le site de Hello Asso";
+export const FACEBOOK_MODAL_TEXT = "En cliquant sur confirmer, nous vous redirigeons vers notre page facebook Nord Est Toulousain en Transition"
